d7c24980931f2fa1e14a23e36f71109b6d182dcd
Sil van Diepen <me@sil.mt>
2021-12-30 09:20:59 +0100

    remove dist

--- dist/api/files.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Order = void 0;
-var Order;
-(function (Order) {
-    Order["ASC"] = "[Order] Ascending";
-    Order["DESC"] = "[Order] Descending";
-})(Order = exports.Order || (exports.Order = {}));
-//# sourceMappingURL=files.model.js.map
